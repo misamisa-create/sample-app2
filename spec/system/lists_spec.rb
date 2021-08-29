9e5d5a52657dab631ecf441e8fa941c75690e140
EC2 Default User <missmis0329@gmail.com>
2021-08-29 14:03:44 +0000

    [add]

---
+++ spec/system/lists_spec.rb
@@ -0,0 +1,85 @@
+#frozen_string_literal: true
+
+require 'rails_helper'
+
+describe '投稿のテスト' do
+  let!(:list){create(:list,title:'hoge',body:'body')}
+  describe 'トップ画面(top_path)へのテスト' do
+    before do
+      visit top_path
+    end
+    context '表示の確認' do
+      it 'トップ画面(top_path)に「ここはTopページです」が表示されているか' do
+        expect(page).to have_content'ここはTopページです'
+      end
+      it 'top_pathが"/top"であるか' do
+        expect(current_path).to eq('/')
+      end
+    end
+  end
+
+  describe '投稿画面(todolists_new_path)のテスト' do
+    before do
+      visit todolists_new_path
+    end
+    context '表示の確認' do
+      it 'todolists_new_pathが"/todolists/new"であるか' do
+        expect(page).to eq("/todolists/new")
+      end
+      it '投稿ボタンが表示されているか' do
+        expect(page).to have_button '投稿'
+      end
+    end
+    context '投稿処理のテスト' do
+      it '投稿後のリダイレクト先は正しいか' do
+        fill_in 'list[title]',with: Faker::Lorem.characters(number:5)
+        fill_in 'list[body]',with: Faker::Lorem.characters(number:20)
+        click_button '投稿'
+        expect(page).have_current_path todolists_path(List.last)
+      end
+    end
+  end
+
+  describe '投稿一覧のテスト' do
+    before do
+      visit todolists_path
+    end
+    context '表示の確認' do
+      it '投稿されたものが表示されているか' do
+	      expect(page).to have_content list.title
+	      expect(page).to have_link list.title
+      end
+    end
+  end
+
+  # これ以降はまだ答え合わせしてない
+  describe '詳細画面のテスト' do
+    before do
+      visit todolist_path(list)
+    end
+    context '表示のテスト' do
+      it '' do
+      end
+    end
+    context 'リンクの遷移先の確認' do
+      it '' do
+      end
+    end
+    context 'list削除のテスト' do
+      it '' do
+      end
+    end
+  end
+
+  describe '編集画面のテスト' do
+    before do
+      visit edit_todolist_path(list)
+    end
+    context '' do
+      it '' do
+      end
+    end
+  end
+
+
+end
