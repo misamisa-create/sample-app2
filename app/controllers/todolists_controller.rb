75c08d29ebc718c6a8fd54362a916dcf133ada84
EC2 Default User <missmis0329@gmail.com>
2021-08-30 08:38:32 +0000

    [add]Rubocop

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,9 +1,8 @@
 class TodolistsController < ApplicationController
-
-
   def new
     @list = List.new
   end
+
   def create
     # １. データを新規登録するためのインスタンス作成
     list = List.new(list_params)
@@ -24,18 +23,21 @@ class TodolistsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
+
   def destroy
-    list = List.find(params[:id])  # データ（レコード）を1件取得
-    list.destroy  # データ（レコード）を削除
-    redirect_to todolists_path  # 投稿一覧画面へリダイレクト
+    list = List.find(params[:id]) # データ（レコード）を1件取得
+    list.destroy # データ（レコード）を削除
+    redirect_to todolists_path # 投稿一覧画面へリダイレクト
   end
 
   private
+
   # ストロングパラメータ
   def list_params
     params.require(:list).permit(:title, :body, :image)
--- spec/factories/list.rb
+++ spec/factories/list.rb
@@ -1,6 +1,6 @@
 Factorybot.define do
-  factory:list do
-    title[Faker::Lorean.characters(number:10)]
-    body[Faker::Lorean.characters(number:30)]
+  factory :list do
+    title[Faker::Lorean.characters(number: 10)]
+    body[Faker::Lorean.characters(number: 30)]
   end
 end
